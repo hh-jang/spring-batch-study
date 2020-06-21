fb8a1722f3b380963bcd86c41ea4a2ced07b6d47
hh-jang <jhh1117@daum.net>
2020-06-21 21:16:13 +0900

    next를 위한 새로운 job 추가, mysql 연동

--- src/main/java/com/hhjang/springbatch/job/StepNextJobConfiguration.java
+++ src/main/java/com/hhjang/springbatch/job/StepNextJobConfiguration.java
@@ -5,47 +5,50 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class SimpleJobConfiguration {
+public class StepNextJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job simpleJob() {
-        return jobBuilderFactory.get("temp")
-                .start(simpleStep1(null))
-                .next(simpleStep2(null))
+    public Job stepNextJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(step1())
+                .next(step2())
+                .next(step3())
                 .build();
     }
 
-    // step 을 구성하는 공식 문서 참조 https://docs.spring.io/spring-batch/docs/current/reference/html/step.html#configureStep
     @Bean
-    @JobScope
-    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info("this is simpleStep1");
-                    log.info("---------request date : "  +requestDate);
+                    log.info("--------------This is step1--------------");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
 
     @Bean
-    @JobScope
-    public Step simpleStep2(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep2")
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-                    log.info("this is simpleStep2");
-                    log.info("---------request date : "  +requestDate);
+                    log.info("--------------This is step2--------------");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("--------------This is step3--------------");
                     return RepeatStatus.FINISHED;
                 }).build();
     }
