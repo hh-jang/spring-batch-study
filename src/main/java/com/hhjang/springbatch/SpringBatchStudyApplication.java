5267393dc80aefb6bb0195478c2575818fc58ce4
hh-jang <jhh1117@daum.net>
2020-06-21 19:58:14 +0900

    simple job, step 추가
    * 기본적인 job, step 생성 및 확인

--- src/main/java/com/hhjang/springbatch/SpringBatchStudyApplication.java
+++ src/main/java/com/hhjang/springbatch/SpringBatchStudyApplication.java
@@ -1,13 +1,14 @@
 package com.hhjang.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing  // 배치 기능 활성화
 @SpringBootApplication
 public class SpringBatchStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchStudyApplication.class, args);
     }
-
 }
