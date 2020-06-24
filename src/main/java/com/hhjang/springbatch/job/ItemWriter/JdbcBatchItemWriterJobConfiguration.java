b54f0f76f61bddb36bf0c72201823b0f5a01743e
hh-jang <jhh1117@daum.net>
2020-06-24 21:42:57 +0900

    ItemWriter example 진행
    * jdbcBatchItemWriter, jpaItemWriter를 이용한 example
    
    * test를 위한 Pay2 추가

---
+++ src/main/java/com/hhjang/springbatch/job/ItemWriter/JdbcBatchItemWriterJobConfiguration.java
@@ -0,0 +1,65 @@
+package com.hhjang.springbatch.job.ItemWriter;
+
+import com.hhjang.springbatch.entity.Pay;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class JdbcBatchItemWriterJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    private static final int MAXIMUM_CHUNK_SIZE = 10;
+
+    @Bean
+    public Job jdbcBatchItemWriterJob() {
+        return jobBuilderFactory.get("jdbcBatchItemWriterJob")
+                .start(jdbcBatchItemWriterStep())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcBatchItemWriterStep() {
+        return stepBuilderFactory.get("jdbcBatchItemWriterStep")
+                .<Pay, Pay>chunk(MAXIMUM_CHUNK_SIZE)
+                .reader(jdbcBatchItemWriterReader())
+                .writer(jdbcBatchItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JdbcBatchItemWriter<Pay> jdbcBatchItemWriter() {
+        return new JdbcBatchItemWriterBuilder<Pay>()
+                .dataSource(dataSource)
+                .sql("insert into pay2(amount, tx_name, tx_date_time) values (:amount, :txName, :txDateTime)")
+                .beanMapped()
+                .build();
+    }
+
+    @Bean
+    public JdbcCursorItemReader<Pay> jdbcBatchItemWriterReader() {
+        return new JdbcCursorItemReaderBuilder<Pay>()
+                .dataSource(dataSource)
+                .fetchSize(MAXIMUM_CHUNK_SIZE)
+                .rowMapper(new BeanPropertyRowMapper<>(Pay.class))
+                .name("jdbcBatchItemWriter")
+                .sql("SELECT id, amount, tx_name, tx_date_time FROM pay")
+                .build();
+    }
+}
